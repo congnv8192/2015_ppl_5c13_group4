42dc763fffb0c478763b8c9aea751e8ede480215
thuytet <thuy7c13hanu@gmail.com>
2015-04-15 09:07:03 -0700

    Nguyễn Thị Thuỷ

---
+++ thuynt/TextProcessor.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class Excercise here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Excercise
+{
+    
+   /**
+    * read text filepath “data.txt”, remove the redundant spaces,first letter of each word is in uppercase and the
+remaining letters are in lowercase
+    * @require filepath != null
+    * @modifiers
+    * @effects
+    * read text file into String 
+    * remove the redundant spaces 
+    * uppercase first letter
+    * lowercase remaining letters
+    * return content
+    */
+   public static String Ex12(String file){
+    }
+   /**
+     * count the number of lines and distinct words
+     * @require file != NULL
+     * @modifiers
+     * @effects
+     * count the line number 
+     * count the distinct words
+     *       same words counted once
+     * return number
+     * /
+     public static void Int Ex3(Int count){
+        }
+   /**
+    * write updated content to another text file named “data2.txt”,keep the same line
+    * @require file != null
+    * @modifiers
+    * @effects
+    * keeping the same line break
+    */
+    public static String Ex3(String file){}
+    
+   /**
+    * display a brief text-based report summarising the results of tasks 2, 3 ,4
+    * @require file != NULL
+    * @modifiers
+    * @effects
+    * summarise the results
+    * dispay a brief text
+    * format
+    *        Total number of original lines : l
+             Total number of original words : w
+             Number of redundant spaces removed : s
+             Number of distinct words : d
+             Updated content written to file : data2.txt
+    */
+   public static String Ex4(Sring file){}
+}
+
