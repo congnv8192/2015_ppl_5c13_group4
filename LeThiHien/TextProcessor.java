499763c013140aaf75e96db1f9f9e2ad32928b2c
Hienlt95 <hienlt95.hanu@gmail.com>
2015-04-15 23:03:21 +0700

    Lê Thị Hiền

---
+++ LeThiHien/TextProcessor.java
@@ -0,0 +1,28 @@
+/**
+     * read file, remove all the redundant spaces, uppercase first letter of each word and lowercase other.
+     * @require filepath != null
+     * @modifies 
+     * @effects 
+     *  read file content into String content
+     *  remove all redundant spaces in content 
+     *  foreach word in content
+     *      capitalize first letter
+     *      lowercase remaining letters
+     *  returns content
+     */
+    public static String task2(String filepath){
+        
+}
+
+   /**
+    * read file, count lines, count words, write updated content into "data2.txt"
+    * @require filepath != null
+    * @modifies
+    * @effect
+    * read file content into String content
+    * count lines of content
+    * count words of content
+    * write updated content into "data2.txt"
+    */
+ public static String task3(Strinh filepath){
+    }
